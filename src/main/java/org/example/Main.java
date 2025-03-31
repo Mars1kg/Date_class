a67e587702127fed8440c6357261231287794eb6
sultan <zyldyzbekovsultan02@gmail.com>
2025-03-31 22:45:40 +0600

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
         System.out.println("Hello!");
     }
 }
